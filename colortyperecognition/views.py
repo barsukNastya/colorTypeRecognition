4f203c2257e5efc2a68d7d6fca3161e31927a17c
Melenteva <barsuk.nastya1@gmail.com>
2018-01-06 00:55:41 +0500

    get data from db

--- colortyperecognition/views.py
+++ colortyperecognition/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import HairColor
 
 def greating(request):
-  return render(request, 'colortyperecognition/greating.html', {})
+  hair_colors = HairColor.objects.all()
+  return render(request, 'colortyperecognition/greating.html', {'hair_colors' : hair_colors})
+
+  # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
